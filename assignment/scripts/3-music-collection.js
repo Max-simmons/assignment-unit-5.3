006973c1f82f0542a701f5931231618207af3be3
Max Simmons <maxsimmons20@gmail.com>
2023-03-26 11:39:03 -0500

    added DS2

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,28 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addtoCollection(title, artist, yearsPublished){
+    collection.push(title, artist, yearsPublished);
+}
+
+let record1 = {
+    title: 'Take Care',
+    artist: 'Drake',
+    yearsPublished: 2011
+}
+
+console.log('collection is empty');
+
+addtoCollection(record1)
+
+console.log('added Take Care to the collection');
+
+let record2 = {
+    title: 'DS2',
+    artist: 'Furture',
+    yearsPublished: 2015
+}
+
+addtoCollection(record2)
+
+console.log('added DS2 to the collection'); 
